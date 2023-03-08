71db65f6d53dc171004de43bb1e539f0d866ab36
Razawaky <viniciusduarte564@gmail.com>
2023-03-08 12:25:20 -0300

    Projeto aula

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+                                    //selecionar todos os buttons e monitorar os clicks
+    $('.btn').click(function(e){  //$ simplificação do getElementBy por causa do Jquery. Jquery tem que ler toda a documentação
+        e.preventDefault() //Serve para controlar e gerenciar uma funcionaliade HTML (Uma função do Jquery)
+        alert('Error')
+        console.log('Uma socilicitação foi dada...')
+    })
+})
